93157a2e611faf5bfd0533f92183763d9f7ed79c
Henock Adane <meetheni@gmail.com>
2021-07-27 14:16:29 -0700

    modified _printf.c and holberton.h

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <limits.h>
 /**
  * _printf - Modified Printf function that produces output with format.
  * @format: references to the desired format to be Printed.
@@ -10,76 +11,81 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-
-	va_start(args, format);
-
 	int count = 0;
 
+	va_start(args, format);
 	while (*format != '\0')
-
 	{
 		if (*format == '%')
-
 		{
 			if (*(format + 1) == '%')
 			{
 				char c = '%';
 
-				write(1, &c, 1);
-				format++;
-				count++;
+				print_char(&c, &format, &count);
 			}
-
 			if (*(format + 1) == 'c')
 			{
 				char st = va_arg(args, int);
 
-				write(1, &st, 1);
-				format++;
-				count++;
-
+				print_char(&st, &format, &count);
 			}
-
 			if (*(format + 1) == 's')
 			{
-
 				char *string;
 				int i;
 
 				string = va_arg(args, char *);
 
-				if (string == NULL)
-				{
-					string = "(null)";
-				}
-
-				for (i = 0; string[i] != '\0'; i++)
-				{
-
-					write(1, string + i, 1);
-					count++;
-
-				}
-
+				print_string(string, &count);
 				format++;
 			}
 		}
-
 		else
 		{
 			write(1, format, 1);
 			count++;
-
 		}
-
 		++format;
 	}
-
 	va_end(args);
-
 	return (count);
 }
+/**
+ * print_string - func that prints a string.
+ * @string: declaration of string pointer with a value to be printed.
+ * @counter: an int counter for the string.
+ * Return: no value.
+ */
+void print_string(char *string, int *counter)
+{
+	int strcount = 0;
 
+	if (string == NULL)
+	{
+		string = "(null)";
+	}
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		write(1, string + i, 1);
+		strcount++;
+	}
+	*counter = *counter + strcount;
+
+}
+/**
+ * print_char - func that prints a character.
+ * @ch: the character to be printed.
+ * @inc1: a value to be incremented for formatting.
+ * @inc2: a value to be incremented for counting.
+ * Return: no value.
+ */
+void print_char(char *ch, int *inc1, int *inc2)
+{
+	write(1, ch, 1);
+	*inc1 = *inc1 + 1;
+	*inc2 = *inc2 + 1;
+}
 
 		}
 	}
--- holberton.h
+++ holberton.h
@@ -1,1 +1,10 @@
-int _printf(const char *format, ...)
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+void print_char(char *ch, int *inc1, int *inc2);
+void print_string(char *string, int *counter);
+#endif
